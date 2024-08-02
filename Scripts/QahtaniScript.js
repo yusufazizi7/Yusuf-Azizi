a7b79d5af6d16fd6031fbc19eef625472607c528
Yusuf Azizi <yusofazizi1955@gmail.com>
2024-08-02 23:36:29 +0800

    updates

--- Scripts/QahtaniScript.js
+++ Scripts/QahtaniScript.js
@@ -30,6 +30,40 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+
+document.addEventListener('DOMContentLoaded', function() {
+    var graphicToggle = document.getElementById('graphicToggle');
+    var graphics = document.querySelectorAll('.graphics'); // Use querySelectorAll to select all elements with the class 'graphics'
+
+    // Function to update the graphics display
+    function updateGraphics() {
+        graphics.forEach(function(graphic) { // Loop through all selected elements
+            if (graphicToggle.checked) {
+                graphic.style.display = '';
+            } else {
+                graphic.style.display = 'none';
+            }
+        });
+    }
+
+    // Load the saved preference from localStorage
+    var graphicTogglePref = localStorage.getItem('showgraphics');
+    if (graphicTogglePref !== null) {
+        graphicToggle.checked = JSON.parse(graphicTogglePref);
+    }
+
+    // Initial update based on checkbox state
+    updateGraphics();
+
+    // Event listener for checkbox changes
+    graphicToggle.addEventListener('change', function() {
+        updateGraphics();
+        // Save the current preference to localStorage
+        localStorage.setItem('showgraphics', graphicToggle.checked);
+    });
+});
+
+
 document.addEventListener('DOMContentLoaded', function() {
     const listItems = document.querySelectorAll('.nooniyah li');
 
