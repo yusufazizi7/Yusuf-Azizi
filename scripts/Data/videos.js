c96bbc49ac0c02cd5178b1ec09d063c4bd931e05
Yusuf Azizi <yusofazizi1955@gmail.com>
2026-09-20 22:55:28 +0800

    renames

--- scripts/Data/videos.js
+++ scripts/Data/videos.js
@@ -13,7 +13,7 @@ window.islamicQalamVideos = [
             "While little is known about the author, this poem which he wrote around about the 5th century has emerged as one of the most powerful, beautiful and well-celebrated works throughout time. It's a piece that scholars from all areas of the Muslim World are familiar with, and parts of it are taught to students as part of their Islamic studies to this day. The poem itself has almost 700 lines (well over 6,000 words) covering many aspects of Islam: Qur'an, 'Aqidah, hadith, fiqh, history, seerah etc. It also goes into clarifying much of the creedal and social problems that arose in the Ummah during that time (e.g. the view of the Mu'tazilah as well as issues brought by other heretical groups) and it continuously gives beautiful advice on how the Muslim should live - e.g. how to deal with your brothers, how to perfect your character, how to achieve in life etc.",
 
         thumbnail:
-            "images/Video-Thumbnails/nooniyah-al-qahtani.jpg",
+            "images/video-thumbnails/nooniyah-al-qahtani.jpg",
 
         video:
             "https://media.islamicqalam.com/videos/nooniyah-al-qahtani-complete-poem.mp4"
@@ -33,7 +33,7 @@ window.islamicQalamVideos = [
             "This video presents a powerful poem by Ibn Al-Qayyim, written as a response to Christian theology. With eloquent verses, he critiques the doctrines of the Trinity and incarnation, affirming the purity of Islamic monotheism.",
 
         thumbnail:
-            "images/Video-Thumbnails/oh-worshippers-of-christ.jpg",
+            "images/video-thumbnails/oh-worshippers-of-christ.jpg",
 
         video:
             "https://media.islamicqalam.com/videos/oh-worshipper-of-christ.mp4"
@@ -53,7 +53,7 @@ window.islamicQalamVideos = [
             "Laamiyah Ibn Al-Wardi is a renowned Arabic poem composed by Imam Zain Al-Din Ibn Al-Wardi. It serves as a timeless piece of wisdom, offering ethical guidance and highlighting the values of humility, self-discipline, and reliance on Allah. Written in a rhythmic and eloquent style, the poem emphasises the transient nature of worldly pleasures and encourages the pursuit of virtuous deeds for ultimate success in the Hereafter. Its concise yet profound verses continue to inspire readers and learners across generations.",
 
         thumbnail:
-            "images/Video-Thumbnails/laamiyah-ibn-al-wardi.jpg",
+            "images/video-thumbnails/laamiyah-ibn-al-wardi.jpg",
 
         video:
             "https://media.islamicqalam.com/videos/laamiyah-ibn-al-wardi.mp4"
@@ -73,7 +73,7 @@ window.islamicQalamVideos = [
             "This Laamiyah which is believed to be a work of Ibn Taymiyyah, explains important topics like belief in the Qur’an, Allah’s attributes, and the status of the Prophet’s companions. The poem is easy to understand and is often memorised by students of knowledge.",
 
         thumbnail:
-            "images/Video-Thumbnails/laamiyah-ibn-taymiyyah.jpg",
+            "images/video-thumbnails/laamiyah-ibn-taymiyyah.jpg",
 
         video:
             "https://media.islamicqalam.com/videos/laamiyah-ibn-taymiyyah.mp4"
@@ -93,7 +93,7 @@ window.islamicQalamVideos = [
             "A powerful poem by Ibn Abi Dawud summarising the core beliefs of Ahl al-Sunnah wal-Jama'ah— including holding to the Qur'an and Sunnah, honouring the Companions, belief in the Divine Attributes, Qadr, and the Afterlife. A timeless guide to correct creed and clarity in faith.",
 
         thumbnail:
-            "images/Video-Thumbnails/haiyyah-ibn-abi-dawud.png",
+            "images/video-thumbnails/haiyyah-ibn-abi-dawud.png",
 
         video:
             "https://media.islamicqalam.com/videos/haiyyah-ibn-abi-dawud.mp4"
@@ -113,7 +113,7 @@ window.islamicQalamVideos = [
             "Ta'iyyah Al-ilbiri is a powerful Arabic poem filled with wisdom, self-reflection, and advice about this world, the Hereafter, and the path of righteousness. The poet reminds the reader of the fleeting nature of the dunya, the dangers of sin, the importance of humility, and the honour of living with zuhd and taqwa. A timeless work of Islamic moral guidance.",
 
         thumbnail:
-            "images/Video-Thumbnails/taiyyah-al-ilbiri.jpg",
+            "images/video-thumbnails/taiyyah-al-ilbiri.jpg",
 
         video:
             "https://media.islamicqalam.com/videos/taiyyah-al-ilbiri.mp4"
@@ -133,7 +133,7 @@ window.islamicQalamVideos = [
             "A powerful poem by Imam Ibn al-Qayyim exposing the falsehood and extremism that crept into the Ummah through the claims of the pseudo-spirituals. He sheds light on their abandonment of the Qur'an and Sunnah in favour of “inspiration,” dancing, and philosophical illusions. This poem isn’t just criticism—it’s a call back to the path of the Salaf, the Qur’an, and the guidance of the Prophet (SAW).",
 
         thumbnail:
-            "images/Video-Thumbnails/laamiyah-ibn-al-qayyim.png",
+            "images/video-thumbnails/laamiyah-ibn-al-qayyim.png",
 
         video:
             "https://media.islamicqalam.com/videos/laamiyah-ibn-al-qayyim.mp4"
@@ -153,7 +153,7 @@ window.islamicQalamVideos = [
             "Al-Aqeedah As-Safariniyyah is a well-known poetic summary of Islamic creed, written by Imam Muhammad Ibn Ahmad As-Safarini. The poem is written in Rajaz metre, which means it follows a rhythmic structure but doesn’t stick to a single rhyme throughout, making it ideal for teaching and memorisation.",
 
         thumbnail:
-            "images/Video-Thumbnails/al-aqeedah-as-safariniyyah.png",
+            "images/video-thumbnails/al-aqeedah-as-safariniyyah.png",
 
         video:
             "https://media.islamicqalam.com/videos/al-aqeedah-as-safariniyyah.mp4"
@@ -173,7 +173,7 @@ window.islamicQalamVideos = [
             "This video presents a powerful admonitory poem by the scholar and poet Abdul Qahir ibn Abdul Wahid Al Tabrizi, may Allah have mercy on him.",
 
         thumbnail:
-            "images/Video-Thumbnails/ainiyyah-at-tabrizi.jpg",
+            "images/video-thumbnails/ainiyyah-at-tabrizi.jpg",
 
         video:
             "https://media.islamicqalam.com/videos/rubaiyyah-at-tabrizi.mp4"
@@ -193,7 +193,7 @@ window.islamicQalamVideos = [
             "This nasheed reflects on martyrdom, honour, and sacrifice in the path of Allah. It contrasts empty tears with true dignity, portraying the lofty rank and eternal reward granted to the martyr. A reminder of courage, faith, and the reality of the Hereafter.",
 
         thumbnail:
-            "images/Video-Thumbnails/poem-about-martyrdom.jpg",
+            "images/video-thumbnails/poem-about-martyrdom.jpg",
 
         video:
             "https://media.islamicqalam.com/videos/poem-about-martyrdom.mp4"
@@ -213,7 +213,7 @@ window.islamicQalamVideos = [
             "This powerful poem was composed by Al Mutanabbi after his escape from Egypt. It narrates a perilous journey across deserts and harsh lands, using vivid imagery of travel, endurance, and danger to reflect inner strength and determination.",
 
         thumbnail:
-            "images/Video-Thumbnails/ala-kullu-mashiah.jpg",
+            "images/video-thumbnails/ala-kullu-mashiah.jpg",
 
         video:
             "https://media.islamicqalam.com/videos/ala-kullu-mashiah.mp4"
@@ -233,7 +233,7 @@ window.islamicQalamVideos = [
             "A profound admonitory poem by Ibn Rajab al-Hanbali that censures the hardness of the heart. Through vivid imagery and direct counsel, the poem exposes the deception of worldly life, the fleeting nature of power and wealth, and the inevitability of death, calling the listener to wakefulness, humility, and sincere preparation for the Hereafter.",
 
         thumbnail:
-            "images/Video-Thumbnails/taiyyah-ibn-rajab.jpg",
+            "images/video-thumbnails/taiyyah-ibn-rajab.jpg",
 
         video:
             "https://media.islamicqalam.com/videos/taiyyah-ibn-rajab.mp4"
@@ -252,7 +252,7 @@ window.islamicQalamVideos = [
             "A powerful poem by Abdul Raheem Mahmoud, expressing unwavering honour, defiance, and sacrifice. Through vivid imagery and resolute language, the poet rejects humiliation and fear, presenting a noble death as preferable to a life without dignity. A timeless declaration of courage, resolve, and steadfast resistance.",
 
         thumbnail:
-            "images/Video-Thumbnails/saahmilu-ruhi-ala-rahati.jpg",
+            "images/video-thumbnails/saahmilu-ruhi-ala-rahati.jpg",
 
         video:
             "https://media.islamicqalam.com/videos/saahmilu-ruhi-ala-rahati.mp4"
@@ -271,7 +271,7 @@ window.islamicQalamVideos = [
             "A powerful poem by Ibn Taymiyyah expressing absolute humility, servitude, and total dependence upon Allah. In these profound verses, he affirms that all creation is inherently poor and in need of its Lord, while Allah alone is perfectly Self-Sufficient. The poem establishes pure Tawhid, negates self-reliance, and reminds us that every good comes only from Him.",
 
         thumbnail:
-            "images/Video-Thumbnails/taiyyah-ibn-taymiyyah.jpg",
+            "images/video-thumbnails/taiyyah-ibn-taymiyyah.jpg",
 
         video:
             "https://media.islamicqalam.com/videos/taiyyah-ibn-taymiyyah.mp4"
@@ -291,7 +291,7 @@ window.islamicQalamVideos = [
             "This moving poem by Ibn Rajab Al-Hanbali reflects on the passing of life, human heedlessness, and the immense virtue of the blessed month of Ramadan. With words of humility and self-accounting, the poet reminds the reader of gratitude, repentance, and the priceless opportunity found in Laylatul Qadr, a night of mercy, forgiveness, and liberation from the Fire.",
 
         thumbnail:
-            "images/Video-Thumbnails/raiyyah-ibn-rajab.jpg",
+            "images/video-thumbnails/raiyyah-ibn-rajab.jpg",
 
         video:
             "https://media.islamicqalam.com/videos/raiyyah-ibn-rajab.mp4"
@@ -310,7 +310,7 @@ window.islamicQalamVideos = [
             "This poem highlights the virtues and honour of Aishah, the Mother of the Believers, defending her purity and status through powerful and eloquent verses. It affirms her closeness to the Prophet (SAW), her knowledge, and her distinguished position among the companions, while calling to love and respect the family and companions of the Prophet.",
 
         thumbnail:
-            "images/Video-Thumbnails/nooniyah-about-aishah.jpg",
+            "images/video-thumbnails/nooniyah-about-aishah.jpg",
 
         video:
             "https://media.islamicqalam.com/videos/nooniyah-about-aishah.mp4"
@@ -329,7 +329,7 @@ window.islamicQalamVideos = [
             "A powerful pre-Islamic poem by Imru’ al-Qais, beginning with “تطاول ليلك بالأثمد”, expressing sleepless grief, tribal honour, vengeance, and preparation for war. A poem filled with pride, courage, and the spirit of the ancient Arabs.",
 
         thumbnail:
-            "images/Video-Thumbnails/tatawala-layluka.jpg",
+            "images/video-thumbnails/tatawala-layluka.jpg",
 
         video:
             "https://media.islamicqalam.com/videos/tatawala-layluka.mp4"
@@ -349,7 +349,7 @@ window.islamicQalamVideos = [
             "A beautiful Islamic poem summarising the Seerah of Prophet Muhammad, mentioning his noble lineage, family, wives, children, and important events from his blessed life. It is a concise and beneficial poem for students of knowledge.",
 
         thumbnail:
-            "images/Video-Thumbnails/al-munirah.jpg",
+            "images/video-thumbnails/al-munirah.jpg",
 
         video:
             "https://media.islamicqalam.com/videos/al-munirah.mp4"
@@ -368,7 +368,7 @@ window.islamicQalamVideos = [
             "A beautiful Islamic poem explaining the foundations of belief according to the Quran and the Sunnah upon the way of the righteous early generations.",
 
         thumbnail:
-            "images/Video-Thumbnails/al-baida.jpg",
+            "images/video-thumbnails/al-baida.jpg",
 
         video:
             "https://media.islamicqalam.com/videos/al-baidah.mp4"
@@ -388,7 +388,7 @@ window.islamicQalamVideos = [
             "This concise and beautifully structured urjuzah recounts the life of Prophet Muhammad from his blessed birth and noble character to prophethood, migration, battles, and passing. Written in a simple poetic style, it presents the major events of the Prophetic biography in a smooth and memorable form, filled with love, reverence, and reminders of faith.",
 
         thumbnail:
-            "images/Video-Thumbnails/urjuzah-about-sirah.jpg",
+            "images/video-thumbnails/urjuzah-about-sirah.jpg",
 
         video:
             "https://media.islamicqalam.com/videos/urjuzah-about-sirah.mp4"
@@ -407,7 +407,7 @@ window.islamicQalamVideos = [
             "The 99 Names of Allah, known as Al-Asma’ Al-Husna, describe Allah’s perfect attributes, such as His mercy, power, wisdom, knowledge, and forgiveness. Learning and reflecting upon these beautiful names helps us better understand and worship Allah. This video contains a beautiful recitation of the 99 Names of Allah, elegant Arabic calligraphy, and the English translation of each name.",
 
         thumbnail:
-            "images/Video-Thumbnails/names-of-allah.jpg",
+            "images/video-thumbnails/names-of-allah.jpg",
 
         video:
             "https://media.islamicqalam.com/videos/names-of-allah.mp4"
